e64f40b18a08fd4d830e25c1f8f137bd85c1fc2b
xm-71 <alexcannon2@live.com>
2016-06-14 09:08:51 -0400

    working note section & form added to the document

---
+++ meganote-master/app/notes/notes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('meganote.notes', [
+  'ui.router'
+])
+
+.config(function($stateProvider) {
+  $stateProvider
+
+  .state('notes', {
+      url: '/notes',
+      templateUrl: 'notes/notes.html',
+      controller: 'NotesController'
+    })
+
+  .state('notes.form', {
+    url: '/:noteId',
+    templateUrl: 'notes/notes-form.html'
+  });
+})
+
+.controller('NotesController', function() {
+});
