0b5b2f3386e62859454331d1fd450b7f79ca9a31
xm-71 <alexcannon2@live.com>
2016-06-15 12:01:50 -0400

    delete button and update button working

--- meganote-master/app/notes/notes.js
+++ meganote-master/app/notes/notes.js
@@ -1,6 +1,5 @@
 (function() {
-  angular
-    .module('meganote.notes', ['ui.router'])
+  angular.module('meganote.notes', ['ui.router'])
     .config(notesConfig)
     .controller('NotesController', NotesController);
 
@@ -8,16 +7,16 @@
   function notesConfig($stateProvider) {
     $stateProvider
 
-      .state('notes', {
-        url: '/notes',
-        templateUrl: 'notes/notes.html',
-        controller: 'NotesController'
-      })
+    .state('notes', {
+      url: '/notes',
+      templateUrl: 'notes/notes.html',
+      controller: 'NotesController'
+    })
 
-      .state('notes.form', {
-        url: '/:noteId',
-        templateUrl: 'notes/notes-form.html'
-      });
+    .state('notes.form', {
+      url: '/:noteId',
+      templateUrl: 'notes/notes-form.html'
+    });
   }
 
   NotesController.$inject = ['$state', '$scope', 'NotesService'];
@@ -29,26 +28,33 @@
         $scope.notes = NotesService.notes;
       });
 
-    $scope.note = { title: '', body: '' };
+    $scope.clearForm = function() {
+      $scope.note = { title: '', body_html: '' };
+    };
 
     $scope.save = function() {
-      NotesService.create($scope.note);
-      $scope.notes.push($scope.note);
-      $scope.note = { title: '', body: '' };
+      if ($scope.note._id) {
+        NotesService.update($scope.note);
+      }
+      else {
+        NotesService.create($scope.note)
+          .then(function(res) {
+            $scope.note = res.data.note;
+          });
+      }
     };
 
     $scope.edit = function(note) {
-      $scope.note = note;
-    };
-
-    $scope.clearForm = function() {
-      $scope.note = { title: '', body: '' };
+      $scope.note = angular.copy(note);
     };
 
     $scope.delete = function() {
-      NotesService.create($scope.note);
-      $scope.notes.push($scope.note);
-      $scope.note = { title: '', body: '' };
+      NotesService.delete($scope.note)
+        .then(function() {
+          $scope.clearForm();
+        });
     };
+
+    $scope.clearForm();
   }
-})();
+}());
--- meganote-master/app/notes/notes.service.js
+++ meganote-master/app/notes/notes.service.js
@@ -5,7 +5,6 @@
   NotesService.$inject = ['$http'];
   function NotesService($http) {
     var service = this;
-
     service.notes = [];
 
     service.getNotes = function() {
@@ -17,17 +16,48 @@
 
       return notesPromise;
     };
+
     service.create = function(note) {
-      var notesPromise =
-      $http.post('https://meganote.herokuapp.com/notes', {
+      var notesPromise = $http.post('https://meganote.herokuapp.com/notes', {
         note: note
       });
 
       notesPromise.then(function(res) {
-        service.notes.unshift(res.data.notes);
+        service.notes.unshift(res.data.note);
       });
+
       return notesPromise;
     };
-    
+
+    service.update = function(note) {
+      var notesPromise = $http.put('https://meganote.herokuapp.com/notes/' + note._id, {
+        note: note
+      });
+
+      notesPromise.then(function(res) {
+        service.removeById(res.data.note._id);
+        service.notes.unshift(res.data.note);
+      });
+
+      return notesPromise;
+    };
+
+    service.delete = function(note) {
+      var notesPromise = $http.delete('https://meganote.herokuapp.com/notes/' + note._id);
+
+      notesPromise.then(function(res) {
+        service.removeById(res.data.note._id);
+      });
+
+      return notesPromise;
+    };
+
+    service.removeById = function(id) {
+      for (var i=0; i < service.notes.length; i++) {
+        if (service.notes[i]._id === id) {
+          return service.notes.splice(i, 1);
+        }
+      }
+    };
   }
-})();
+}());
